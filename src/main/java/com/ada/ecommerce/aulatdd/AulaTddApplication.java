83e386dc3aef66c1f81bd87a7aaf97231ea06794
Rachel <rachellizandra@outlook.com>
2023-04-23 17:46:02 -0300

    transfere aplicação para pasta principal

--- src/main/java/com/ada/ecommerce/aulatdd/AulaTddApplication.java
+++ src/main/java/com/ada/ecommerce/aulatdd/AulaTddApplication.java
@@ -2,8 +2,10 @@ package com.ada.ecommerce.aulatdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
 public class AulaTddApplication {
 
 	public static void main(String[] args) {
